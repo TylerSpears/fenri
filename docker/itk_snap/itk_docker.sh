99a2d6aac232adc754b9d846cba4a6895dcd2669
Tyler Spears <tylera117@gmail.com>
2021-02-02 15:59:15 -0500

    Add working ITK-SNAP Dockerfile, documentation
    - What change does this apply?
    Creates working version of the ITK-SNAP Dockerfile (and corresponding
    image). Adds some documentation to the Dockerfile, as well as a
    README.
    
    Some minor folder cleanup, as well.
    
    - Why is this change necessary?
    
    - What are the next planned changes?
    Mount a cache directory for itk-snap, so workspace info & file history
    can persist between images.

--- docker/itk_snap/itk_docker.sh
+++ docker/itk_snap/itk_docker.sh
@@ -1,29 +1,17 @@
 
 x11docker \
-        --gpu \
-        --home \
-        --clipboard \
-        --interactive \
-        --pull=ask \
-        --runtime nvidia \
-        --hostipc \
-        -- \
-        --env QT_GRAPHICSSYSTEM="native" \
-        --env DISPLAY=unix$DISPLAY \
-        --env NVIDIA_VISIBLE_DEVICES="all" \
-        --env NVIDIA_DRIVER_CAPABILITIES="graphics,utility,compute" \
-        --gpus all \
-        -- \
-        dorianps/itksnap@sha256:17aaead20d9f56778b662e8cf549453df6340e9d3cf1402abcda75d7ee6c7685
-
-# docker run --rm -it \
-#        --privileged \
-#        --env QT_GRAPHICSSYSTEM="native" \
-#        --env DISPLAY=unix$DISPLAY \
-#        --env NVIDIA_VISIBLE_DEVICES="all" \
-#        --env NVIDIA_DRIVER_CAPABILITIES="graphics,utility,compute" \
-#        --volume /tmp/.X11-unix:/tmp/.X11-unix \
-#        --volume $XAUTHORITY:$XAUTHORITY \
-#        --ipc host \
-#        --gpus all \
-#        dorianps/itksnap@sha256:17aaead20d9f56778b662e8cf549453df6340e9d3cf1402abcda75d7ee6c7685
+    --hostipc \
+    --clipboard \
+    --user=RETAIN \
+    --hostdisplay \
+    --group-add video --group-add render \
+    --gpu \
+    --runtime=nvidia \
+    -- \
+    --rm \
+    --gpus=all \
+    --ipc=host \
+    --volume /srv/data:/srv/data \
+    --volume /srv/tmp:/srv/tmp \
+    -- \
+    mytk
