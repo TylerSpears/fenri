400800fcf7ab97734811dfce611287eb3bc04099
Tyler Spears <tylera117@gmail.com>
2021-02-08 14:26:17 -0500

    Add dockerfile for full flashc compilation
    - What change does this apply?
    Fixes libcuda issues (given initial testing, at least). Changes boil
    down to:
    * stop trying to run unit tests in the build phase
    * move the `/usr/.../stubs` path to the *end* of `LD_LIBRARY_PATH`
    * Explicitly include the stubs path in the `cmake` command for PyCA
    * Symlink `libcuda.so.1 -> libcuda.so` within the stubs file
    * Add stubs to the ldconfig run file
    * Remove the symlink and revert `LD_LIBRARY_PATH` before finishing the
      build.
    
    See:
    <https://github.com/NVIDIA/nvidia-docker/issues/775>
    <https://github.com/tensorflow/tensorflow/pull/44732>
    <https://github.com/tensorflow/tensorflow/issues/25865#issuecomment-473486069>
    <https://github.com/NVIDIA/nvidia-docker/issues/508>
    
    - Why is this change necessary?
    Some fixes may not be necessary, but it did work. As I understand it,
    the stubs dir contains the shared libraries necessary for dynamic
    linking at build-time. However, these files disappear (or change?) when
    the container is actually run.
    
    - What are the next planned changes?
    Cut off some of the bloat with extra packages; multi-stage builds may
    come in handy. Pin commits to the git repo setup. Clean up interface
    with flashc executables; there is no documentation regarding the
    functions. Even though we can use them, we still don't know how.

--- docker/flashc/run_container.sh
+++ docker/flashc/run_container.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 echo "$1"
 if test -z "$1"
-                # --rm 
 then
         docker run \
-                --gpus=all --ipc=shareable \
+                --rm --gpus=all --ipc=shareable \
                 flashc:v0
 else
         docker run \
-                --gpus=all --ipc=shareable \
+                --rm --gpus=all --ipc=shareable \
                 flashc:v0 $1
 fi
