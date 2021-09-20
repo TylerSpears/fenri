fc22081008238843a103cf893ecc08948fe41821
Tyler Spears <tylera117@gmail.com>
2021-09-20 13:05:49 -0400

    Add & test LPTN discriminator net

---
+++ lib/nn/gan/adversarial.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import itertools
+
+import torch
+import torch.nn.functional as F
+
+from .. import layers
+
+
+class DiscriminatorSubNet(torch.nn.Module):
+    def __init__(self, num_input_channels: int):
+        super().__init__()
+
+        self.block1 = layers.DiscriminatorBlock(num_input_channels, 16, normalize=False)
+        self.block2 = layers.DiscriminatorBlock(16, 32, normalize=True)
+        self.block3 = layers.DiscriminatorBlock(32, 64, normalize=True)
+        self.block4 = layers.DiscriminatorBlock(64, 128, normalize=True)
+        self.block5 = layers.DiscriminatorBlock(128, 128, normalize=True)
+        # If the input to the final conv is between (kernel_size, kernel_size + 2), then
+        # just chop off the extra values with the 'valid' padding method.
+        # ! This assumes the conv input size is not >= kernel_size + 2, because we
+        # ! are trying to produce a 1-dimensional output in the end.
+        self.conv_end = torch.nn.Conv3d(128, 1, kernel_size=8, padding="valid")
+
+    def forward(self, x):
+        y = self.block1(x)
+        y = self.block2(y)
+        y = self.block3(y)
+        y = self.block4(y)
+        y = self.block5(y)
+        # If y is smaller than the final conv layer's kernel size, then pad y to equal
+        # the kernel size. If y is larger than the kernel size, then padding should be 0
+        # and the final conv layer should handle the truncation.
+        pad_amt = torch.clamp_min(
+            torch.as_tensor(self.conv_end.kernel_size) - torch.as_tensor(y.shape[-3:]),
+            0,
+        )
+        # Calculate upper and lower padding amounts.
+        pad_lower = torch.floor(pad_amt / 2).int().tolist()
+        pad_upper = torch.ceil(pad_amt / 2).int().tolist()
+        # Reverse the dimensions to match pytorch's crazy padding order.
+        pad_lower = reversed(pad_lower)
+        pad_upper = reversed(pad_upper)
+        # Pair the lower and upper padding amounts with each dimension, flatten the
+        # list.
+        padding = list(itertools.chain.from_iterable(zip(pad_lower, pad_upper)))
+        y = F.pad(y, pad=padding, mode="replicate")
+
+        y = self.conv_end(y)
+
+        return y
+
+
+class MultiDiscriminator(torch.nn.Module):
+    def __init__(
+        self, num_input_channels: int, discrim_downsample_factors: list = [1, 2, 4]
+    ):
+        super().__init__()
+        self._num_input_channels = num_input_channels
+        self._downsample_factors = list(discrim_downsample_factors)
+        self._scale_factors = list(map(lambda x: x ** -1, self._downsample_factors))
+        discriminators = list()
+        for _ in self._downsample_factors:
+            discriminators.append(DiscriminatorSubNet(self._num_input_channels))
+
+        self.discriminators = torch.nn.ModuleList(discriminators)
+
+    @staticmethod
+    def _downsample(x, scale_factor: int):
+        if scale_factor == 1:
+            return x
+        else:
+            return F.interpolate(x, scale_factor=scale_factor, mode="area")
+
+    def forward(self, x):
+        y = list()
+        for i_scale, scale_factor in enumerate(self._scale_factors):
+            x_down = self._downsample(x, scale_factor)
+            y_i = self.discriminators[i_scale](x_down)
+            # Flatten prediction into B x 1 outputs.
+            y.append(y_i.reshape(-1, 1))
+
+        # Combine predictions for each scale's sub-network into a B x N_scales output.
+        y = torch.cat(y, dim=1)
+        return y
--- lib/nn/layers.py
+++ lib/nn/layers.py
@@ -296,107 +296,28 @@ class HighFreqTranslateNet(torch.nn.Module):
         return y, m
 
 
-class LPTN(torch.nn.Module):
-    def __init__(self, num_input_channels, num_high_freq_levels=3):
-        """Laplacian Pyramid Translation Network implementation.
-
-        Parameters
-        ----------
-        num_input_channels : int
-
-        num_pyramid_levels : int, optional
-            Number of high-frequency levels in the pyramid, by default 3.
-
-            Must be >= 1. Note that this is only for the number of *high frequency*
-            levels; the total number of levels will always be num_high_freq_levels + 1,
-            for the low-frequency residual.
-        """
-
+class DiscriminatorBlock(torch.nn.Module):
+    def __init__(
+        self, num_input_channels: int, num_output_channels: int, normalize: bool = True
+    ):
         super().__init__()
-
-        self.num_input_channels = num_input_channels
-
-        self.laplace_pyramid = LaplacePyramid3d(num_high_freq_levels)
-        self.low_freq_net = LowFreqTranslateNet(
-            self.num_input_channels, num_residual_blocks=5
+        self.conv = torch.nn.Conv3d(
+            num_input_channels, num_output_channels, kernel_size=3, stride=2, padding=1
         )
 
-        # Set up the high-frequency processing networks.
-        high_freq_nets = list()
-        high_freq_nets.append(
-            HighFreqTranslateNet(self.num_input_channels * 3, num_residual_blocks=3)
-        )
-        for _ in range(num_high_freq_levels - 1):
-            high_freq_nets.append(HighFreqTranslateNet(1, num_residual_blocks=0))
-        # Reverse to put in order of decreasing frequency, to match the laplacian
-        # pyramid.
-        self.high_freq_nets = torch.nn.ModuleList(reversed(high_freq_nets))
+        if normalize:
+            self.normalizer = torch.nn.InstanceNorm3d(num_output_channels, affine=True)
+        else:
+            self.normalizer = None
 
-    @staticmethod
-    def _ensure_5d(vol: torch.Tensor) -> torch.Tensor:
-        """Reshapes tensor to be `B x C x D x H x W`.
-
-        Parameters
-        ----------
-        vol : torch.Tensor
-
-        Returns
-        -------
-        torch.Tensor
-            Reshaped tensor, with same data as the input.
-        """
-
-        if vol.ndim < 3 or vol.ndim > 5:
-            raise RuntimeError(
-                f"ERROR: volume shape {tuple(vol.shape)} invalid, "
-                + "expected 3 or 5 dimensions."
-            )
-
-        if vol.ndim == 4:
-            raise RuntimeError(
-                "ERROR: volume with 4 dimensions is ambiguous, "
-                + "reshape to 5 dimensions."
-            )
-
-        if vol.ndim == 3:
-            vol = vol[None, None, ...]
-
-        return vol
-
-    def upsample(self, x):
-        # Convenience function, for brevity and in case we want to change the upsampling
-        # method.
-        return F.interpolate(x, scale_factor=2, mode="trilinear")
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-
-        x = self._ensure_5d(x)
-        high_freqs, low_freq = self.laplace_pyramid.decompose(x)
-
-        low_freq_reconstruct = self.low_freq_net(low_freq)
-
-        # Refine high-frequency components.
-        # Prepare first lower-level high-freq input, which is different than all the
-        # other levels.
-        upsample_low_freq = self.upsample(low_freq)
-        upsample_low_freq_reconstruct = self.upsample(low_freq_reconstruct)
-        high_freq_km1_input = torch.cat(
-            [high_freqs[-1], upsample_low_freq, upsample_low_freq_reconstruct], dim=1
-        )
-        high_freqs_reconstruct = [
-            None,
-        ] * len(high_freqs)
-
-        for k, high_freq_net_k in reversed(list(enumerate(self.high_freq_nets))):
-            laplacian_level_k, mask_k = high_freq_net_k(
-                high_freqs[k], high_freq_km1_input
-            )
-            high_freqs_reconstruct[k] = laplacian_level_k
-            if k > 0:
-                high_freq_km1_input = self.upsample(mask_k)
-
-        y = self.laplace_pyramid.reconstruct(
-            high_freqs_reconstruct, low_freq_reconstruct
-        )
+    def forward(self, x):
+        y = self.conv(x)
+        if self.normalizer:
+            # Normalization requires spatial inputs greater than size 1, which can
+            # happen when several discriminator blocks are performing subsequent
+            # downsampling (stride > 1).
+            if torch.prod(torch.as_tensor(y.shape[-3:])) > 1:
+                y = self.normalizer(y)
+        y = F.leaky_relu(y, negative_slope=0.2)
 
         return y
