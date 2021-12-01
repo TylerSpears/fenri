60831270d0ec244b7a09d03a04343fcf1c354aeb
Tyler Spears <tylera117@gmail.com>
2021-12-01 15:00:10 -0500

    Set up python install package

--- pitn/__init__.py
+++ pitn/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
+__version__ = '0.1.0'
 from . import io, samplers, transforms, viz, nn, data, utils, coords
---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Taken from <https://stackoverflow.com/a/62983901>
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
