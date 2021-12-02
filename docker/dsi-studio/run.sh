6a04e2e5bd7dd62ce412ee1effae4d19dacd90f2
Tyler Spears <tylera117@gmail.com>
2021-12-02 14:48:14 -0500

    Add .desktop files for itk, dsi

--- docker/dsi-studio/run.sh
+++ docker/dsi-studio/run.sh
@@ -2,10 +2,11 @@
 
 TMP_DATA_DIR="${TMP_DATA_DIR:-/srv/tmp}"
 DATA_DIR="${DATA_DIR:-/srv/data}"
+LOCAL_DIR="${LOCAL_DIR:-$HOME/.local/share/dsi-studio}"
 DSI_CONFIG_DIR="${DSI_CONFIG_DIR:-$HOME/.config/dsi-studio}"
 DSI_CACHE_DIR="${DSI_CACHE_DIR:-$HOME/.cache/dsi-studio}"
 
-mkdir --parents $DSI_CONFIG_DIR $DSI_CACHE_DIR
+mkdir --parents $DSI_CONFIG_DIR $DSI_CACHE_DIR $LOCAL_DIR
 
 case $1 in
     nvidia)
@@ -34,7 +35,8 @@ x11docker \
     --ipc=host \
     --volume "$DSI_CONFIG_DIR":/home/guest/.config \
     --volume "$DSI_CACHE_DIR":/home/guest/.cache \
+    --volume "$LOCAL_DIR":/home/guest/.local/share/dsi-studio \
     --volume "$TMP_DATA_DIR":/srv/tmp \
     --volume "$DATA_DIR":/srv/data \
     -- \
-    tylerspears/dsi-studio:$IMG_TAG
+    tylerspears/dsi-studio:$IMG_TAG "$@"
