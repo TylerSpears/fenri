7f1b7823ae0a8c3102ecf01235836a06286c06e7
Tyler Spears <tylera117@gmail.com>
2022-01-13 17:21:01 -0500

    Fix conda env export script

--- environment/export_conda_env.sh
+++ environment/export_conda_env.sh
@@ -1,6 +1,7 @@
 # Make sure to "source" this file, not "dot-slash" execute it!
 # Use sed to remove the "prefix:" field from the environment.yml file.
-conda env export | sed '/prefix:/d' > environment.yml
+ACTIVE_PREFIX=$(conda info --json | jq -e -r '.active_prefix')
+conda env export -p "$ACTIVE_PREFIX" | sed '/prefix:/d' > environment.yml
 conda-lock lock \
     --mamba \
     --strip-auth \
