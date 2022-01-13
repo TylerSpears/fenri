2a9bab8bb8fe9e50c490aa15d8488f7b75bd824a
Tyler Spears <tylera117@gmail.com>
2022-01-13 17:24:14 -0500

    Fix null conda env name

--- environment/export_conda_env.sh
+++ environment/export_conda_env.sh
@@ -1,7 +1,10 @@
 # Make sure to "source" this file, not "dot-slash" execute it!
 # Use sed to remove the "prefix:" field from the environment.yml file.
 ACTIVE_PREFIX=$(conda info --json | jq -e -r '.active_prefix')
-conda env export -p "$ACTIVE_PREFIX" | sed '/prefix:/d' > environment.yml
+conda env export -p "$ACTIVE_PREFIX" \
+    | sed '/prefix:/d' \
+    | sed 's/name: null/name: pitn/' \
+    > environment.yml
 conda-lock lock \
     --mamba \
     --strip-auth \
